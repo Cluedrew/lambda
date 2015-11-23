809053d6ce85926b7fb168b2ddda3e277944d65a
Cluedrew <cluedrew@siductionbox>
2015-11-23 15:02:44 -0500

    Quick update to the SROp class documentation.

--- code/sr-op.hpp
+++ code/sr-op.hpp
@@ -1,10 +1,19 @@
 #ifndef SR_OP_HPP
 #define SR_OP_HPP
 
-/* A structure definiting a shift or reduce operation.
+/* A structure definiting a shift/reduce operation.
+ *   There are actually four kinds of shift/reduce operations, one (error)
+ * is implicaty stored by the absince of a op. Hence no repersentation for it
+ * exists in the class.
+ *   Done operations are a signal that we should be done parsing.
+ *   Shift operations add the lookahead symbol to the stack and change the
+ * state to the destination.
+ *   Reduce operations removed a sequence of symbols and apply a reduction
+ * rule to them, creating a single lookahead symbol.
  *
- * They are stored with regards to their origin state and the value of the
- * look ahead so that information is not stored here again.
+ *   Note that they don't actually apply these operations, simply mark that
+ * they should take place. Similarly they don't state when the operation
+ * should occur, they have to be looked up at the approprate time.
  */
 
 struct SROp
@@ -17,7 +26,7 @@ struct SROp
     //error
   } type;
 
-  StateT to;
+  StateT to; // Union with by: a look up for the reduction rule.
 
   bool isShift ();
   bool isReduce ();
