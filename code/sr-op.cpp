a35e65437a520cf2d01ea5c03a3fcc012dcecb8a
Cluedrew <cluedrew@siductionbox>
2015-12-19 19:01:58 -0500

    Worked the SROp class, including i/o and the four subtypes.

--- code/sr-op.cpp
+++ code/sr-op.cpp
@@ -1,36 +1,133 @@
 #include "sr-op.hpp"
 
-// Constructors
-SROp SROp::shiftOp (StateT destination)
-...
+// Implementation for SROp
+// for error use: (char*)value? Little bit danguours.
 
-SROp SROp::reduceOp (...)
-...
+#include <stdexcept>
+#include <string>
+#include <ios>
+#include <istream>
+#include <ostream>
+
+// Constructors and Deconstructor ============================================
+// Hidden Constructor
+SROp::SROp (SROpType t, unsigned int v) :
+  type(t), value(v)
+{}
+
+// Copy Constructor
+SROp::SROp (SROp const & other) :
+  type(other.type), value(other.value)
+{}
+
+// Default Constructor - creates error message.
+SROp::SROp () :
+  type(error), value(0)
+{}
+
+SROp::~SROp ()
+{}
+
+// These named constructors are intended for use by the user.
+SROp SROp::shiftOp (StateT destination)
+{ return SROp(shift, destination); }
+
+SROp SROp::reduceOp (unsigned int ruleNumber)
+{ return SROp(reduce, ruleNumber); }
 
 SROp SROp::doneOp ()
-...
+{ return SROp(done, 0); }
 
-// Predicate Functions
-bool SROp::isShift ()
+SROp SROp::errorOp ()
+{ return SROp(error, 0); }
+
+// Type Check Functions ======================================================
+SROp::SROpType SROp::getType () const
+{ return type; }
+
+bool SROp::isShift () const
 { return shift == type; }
 
-bool SROp::isReduce ()
+bool SROp::isReduce () const
 { return reduce == type; }
 
-bool SROp::isDone ()
+bool SROp::isDone () const
 { return done == type; }
 
-// Getters
-StateT getDest ()
+bool SROp::isError () const
+{ return error == type; }
+
+// Getters Functions =========================================================
+StateT SROp::getDest () const
 {
   if (shift != type)
-    throw ...;
-  return to;
+    throw std::logic_error("SROp: getDest: operation not a shift");
+  return value;
 }
 
-_reduce_type_ getRule ()
+unsigned int SROp::getRule () const
 {
   if (reduce != type)
-    throw ...;
-  return by;
+    throw std::logic_error("SROp: getRule: operation not a reduce");
+  return value;
+}
+
+// Input and Output Operators ================================================
+std::string const shiftStr("shift");
+std::string const reduceStr("reduce");
+std::string const doneStr("done");
+std::string const errorStr("error");
+
+// Output
+std::ostream & operator<< (std::ostream & out, SROp const & op)
+{
+  switch (op.getType())
+  {
+  case SROp::shift:
+    out << std::string("shift ") << op.getDest();
+    break;
+  case SROp::reduce:
+    out << std::string("reduce ") << op.getRule();
+    break;
+  case SROp::done:
+    out << std::string("done");
+    break;
+  case SROp::error:
+    out << std::string("error");
+    break;
+  }
+  return out;
+}
+
+// Input
+std::istream & operator>> (std::istream & in, SROp & op)
+{
+  std::string str;
+  in >> str;
+  if (str == "shift")
+  {
+    StateT dest;
+    in >> dest;
+    op = SROp::shiftOp(dest);
+  }
+  else if (str == "reduce")
+  {
+    unsigned int rule;
+    in >> rule;
+    op = SROp::reduceOp(rule);
+  }
+  else if (str == "done")
+  {
+    op = SROp::doneOp();
+  }
+  else if (str == "error")
+  {
+    op = SROp::errorOp();
+  }
+  else
+  {
+    // I hope bad is the right flag, not fail.
+    in.setstate (std::ios_base::badbit);
+  }
+  return in;
 }
--- code/sr-op.hpp
+++ code/sr-op.hpp
@@ -14,7 +14,14 @@
  *   Note that they don't actually apply these operations, simply mark that
  * they should take place. Similarly they don't state when the operation
  * should occur, they have to be looked up at the approprate time.
+ *
+ *   There are several constructors, they shouldn't be called, use the named
+ * constructors (static functions that return SROps) which should be used
+ * instead.
  */
+
+#include <iosfwd>
+#include "parse-fwd.hpp"
 
 struct SROp
 {
@@ -22,34 +29,85 @@ struct SROp
   {
     shift,
     reduce,
-    done//,
-    //error
-  } type;
-
-  union
-  {
-    StateT shiftTo;
-    // Is this to 'brittle'? It will break if the rules are ever moved.
-    Rule * reduceBy;
+    done,
+    error
   };
 
-  bool isShift ();
-  bool isReduce ();
-  bool isDone ();
+  static SROp shiftOp (StateT destination);
+  /* Create a new shift SROp and return it.
+   * Params: The state that the parser should be in after shifting.
+   * Return: A new SROp object that isShift.
+   */
+
+  static SROp reduceOp (unsigned int ruleNumber);
+  /* Create a new reduce SROp and return it.
+   * Params: The number (position) of a rule within the
+   * Return: A new SROp object that isReduce.
+   */
+
+  static SROp doneOp ();
+  /* Create a new done SROp and return it.
+   * Return: A new SROp object that isDone.
+   */
+
+  static SROp errorOp ();
+  /* Create a new error SROp and return it.
+   * Return: A new SROp object that isError.
+   */
+
+  SROpType getType() const;
+  /* Get the type of SROp.
+   * Return: The SROpType of this SROp.
+   */
+
+  bool isShift () const;
+  bool isReduce () const;
+  bool isDone () const;
+  bool isError () const;
   /* Check to see if it is a shift or a reduce operation.
    * Return: True if the operation is of the given type, false if it is
    *   of a different other type.
    */
 
-  SROp shiftOp (StateT destination);
-  // Create a new shift SROp and return it.
+  StateT getDest () const;
+  /* Get the destination state of this shift operation.
+   * Return: The state that should be transphered to as part of the shift.
+   * Except: Throws std::logic_error if this is not a shift operation.
+   */
 
-  SROp reduceOp (Rule * proRule);
-  // Create a new reduce SROp and return it.
+  unsigned int getRule () const;
+  /* Get the rule used in the reduction.
+   * Return: The rule number that is used in this reduction.
+   * Except: Throws std::logic_error if this is not a reduce operation.
+   */
 
-  static SROp doneOp ()
-  { return SROp{done, 0}; }
-  // Get a new done SROp.
+  SROp (); // Default constructor creates an error op.
+  SROp (SROp const &);
+  ~SROp ();
+private:
+  SROpType type;
+  unsigned int value;
+  SROp (SROpType, unsigned int);
 };
+
+std::ostream & operator<< (std::ostream &, SROp const &);
+/* Print an SROp to a stream.
+ * Params: A reference to the output stream to print to and a constant
+ *   reference to the SROp to print.
+ * Effect: Stream is printed to.
+ * Return: A reference to the output stream.
+ * Format: (shift DEST|reduce RULE|done|error)
+ *   DEST is the result of getDest, RULE is the result of getRule.
+ */
+std::istream & operator>> (std::istream &, SROp &);
+/* Read an SROp from a stream.
+ * Params: A reference to the output stream to read from and a constant
+ *   reference to the SROp to store the value in.
+ * Effect: Stream is read from, the SROp is modified, badbit of the stream
+ *   is set if an error occurs.
+ * Return: A reference to the output stream.
+ * Format: (shift DEST|reduce RULE|done|error)
+ *   DEST is the result of getDest, RULE is the result of getRule.
+ */
 
 #endif//SR_OP_HPP
